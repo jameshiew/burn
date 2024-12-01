d570df0a6ae0648be20d481d0d13d78194061791
James Hiew <james@hiew.net>
2024-12-01 18:58:57 +0000

    Fixing up

--- crates/burn-import/src/onnx/op_configuration.rs
+++ crates/burn-import/src/onnx/op_configuration.rs
@@ -224,14 +224,11 @@ pub fn max_pool2d_config(curr: &Node) -> MaxPool2dConfig {
 }
 
 pub fn onehot_config(node: &Node) -> usize {
-    let mut num_classes: i64 = 0;
+    let mut axis: i64 = 0;
 
     // check if the node has only one input
-    if node.inputs.len() != 1 {
-        panic!(
-            "Onehot: multiple inputs are not supported (got {:?})",
-            node.inputs.len()
-        );
+    if node.inputs.len() != 3 {
+        panic!("Onehot: expected 3 inputs (got {:?})", node.inputs.len());
     }
 
     // extract the shape of the input tensor
@@ -243,35 +240,17 @@ pub fn onehot_config(node: &Node) -> usize {
     // extract the attributes
     for (key, value) in node.attrs.iter() {
         match key.as_str() {
-            "axis" => num_classes = value.clone().into_i64(),
-            "select_last_index" => {
-                // not all params are supported in burn
-                if value.clone().into_i64() != 0 {
-                    log::warn!(
-                        "only select_last_index=0 is supported for argmax in burn. Ignoring supplied value (got {:?})",
-                        value
-                    );
-                }
-            }
-            "keepdims" => {
-                // not all params are supported in burn
-                if value.clone().into_i64() != 1 {
-                    panic!(
-                        "Only keepdims=1 is supported for argmax in burn (got {:?})",
-                        value
-                    );
-                }
-            }
+            "axis" => axis = value.clone().into_i64(),
             _ => {}
         }
     }
 
     // if axis is negative, it is counted from the end
-    if num_classes < 0 {
-        num_classes += tensor.dim as i64;
+    if axis < 0 {
+        axis += tensor.dim as i64;
     }
 
-    num_classes as usize
+    axis as usize
 }
 
 pub fn conv_transpose1d_config(curr: &Node) -> ConvTranspose1dConfig {
